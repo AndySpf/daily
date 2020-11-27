574137f80fa1a7d0da43249581a2feb36200e3f4
AndySpf <spf1316281764@163.com>
2020-11-27 17:50:14 +0800

    add

---
+++ leetcode/algorithm/454_四数相加 II.go
@@ -0,0 +1,35 @@
+package algorithm
+
+// 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，
+// 使得 A[i] + B[j] + C[k] + D[l] = 0。
+// 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。
+// 所有整数的范围在 -228 到 228 - 1 之间，最终结果不会超过 231 - 1 。
+func fourSumCount(A []int, B []int, C []int, D []int) int {
+	m1 := map[int]int{}
+	for i := range A {
+		for j := range B {
+			if _, ok := m1[A[i]+B[j]]; !ok {
+				m1[A[i]+B[j]] = 0
+			}
+			m1[A[i]+B[j]]++
+		}
+	}
+
+	m2 := map[int]int{}
+	for i := range C {
+		for j := range D {
+			if _, ok := m2[C[i]+D[j]]; !ok {
+				m2[C[i]+D[j]] = 0
+			}
+			m2[C[i]+D[j]]++
+		}
+	}
+
+	num := 0
+	for key := range m1 {
+		if _, ok := m2[0-key]; ok {
+			num += m1[key] * m2[0-key]
+		}
+	}
+	return num
+}
--- note/golang编译参数说明.md
+++ note/golang编译参数说明.md
@@ -1,10 +1,11 @@
 go build -gcflags='-m=2' 可以通过查看编译器的报告来了解是否发生了内存逃逸,-m共有四个级别，超过2的内容太多一般用-m=2  
 go run -gcflags '-m -l' -l为关闭inline  
 go build -ldflags "-X 'main.VERSION=$(VERSION)'" 可向main文件中注入参数值  
+go build -ldflags "-s -w" -s为去掉符号表,panic时候的stack trace就没有任何文件名/行号信息了，这个等价于普通C/C++程序被strip的效果。-w为关闭调试符号，禁止debug调试     
 go build -v 编译时显示包名  
 go build -p n 开启并发编译，n默认值为cpu逻辑核数    
 go build -a 强制重新构建  
 go build -n 打印编译时会用到的命令，但不真正执行    
 go build -x 打印编译时会用到的命令
-go build -race 开启静态检测，如果有竞态监，则在运行程序时会有WARN 
+go build -race 开启竞态检测，如果存在竞争，则在运行程序时会有WARN 
 go tool compile -S main.go 获取汇编代码
